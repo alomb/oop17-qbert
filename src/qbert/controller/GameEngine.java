c2aeabafc80473ee93c932ed90e79b842f3a25aa
ale <alessandro.lombardi5@studio.unibo.it>
2018-02-24 18:07:13 +0100

    Creation of the GameLauncher and GameEngine

---
+++ src/qbert/controller/GameEngine.java
@@ -0,0 +1,80 @@
+package qbert.controller;
+
+/**
+ *
+ */
+public class GameEngine {
+
+    private static final long PERIOD = 20;
+
+    private boolean running;
+    private boolean stopped;
+
+    /**
+     * 
+     */
+    public GameEngine() {
+
+    }
+
+    /**
+     * 
+     */
+    public void setup() {
+        this.running = true;
+        this.stopped = false;
+    }
+
+    /**
+     * 
+     */
+    public void mainLoop() {
+        long lastTime = System.currentTimeMillis(); 
+        while (this.running) {
+            final long current = System.currentTimeMillis();
+            final int elapsed = (int) (current - lastTime);
+            this.processInput();
+            this.gameUpdate(elapsed);
+            this.gameRender();
+            this.waitForNextFrame(current);
+            lastTime = current;
+        }
+    }
+
+    /**
+     * @param current
+     */
+    private void waitForNextFrame(final long current) {
+        final long dt = System.currentTimeMillis() - current;
+        if (dt < GameEngine.PERIOD) {
+            try {
+                Thread.sleep(GameEngine.PERIOD - dt);
+            } catch (final Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * 
+     */
+    private void processInput() {
+
+    }
+
+    /**
+     * @param elapsed
+     */
+    private void gameUpdate(final float elapsed) {
+        if (!this.stopped) {
+
+        }
+    }
+
+    /**
+     * 
+     */
+    private void gameRender() {
+
+    }
+}
---
+++ src/qbert/launcher/QBertLauncher.java
@@ -0,0 +1,19 @@
+package qbert.launcher;
+
+/**
+ * The class used to launch the game.
+ */
+public final class QBertLauncher {
+
+    private QBertLauncher() {
+
+    }
+
+    /** 
+     * The main method for the game launcher. 
+     * @param args the parameters passed by command line.
+     */
+    public static void main(final String[] args) {
+
+    }
+}
