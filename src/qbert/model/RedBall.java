59ef375fafcbcb5f185db635c06a6016132ed7f1
ale <alessandro.lombardi5@studio.unibo.it>
2018-03-02 20:42:57 +0100

    Adds RedBall

---
+++ src/qbert/model/RedBall.java
@@ -0,0 +1,24 @@
+package qbert.model;
+
+import qbert.model.states.MoveState;
+import qbert.model.states.SimpleStandingState;
+import qbert.model.utilities.Position2D;
+import qbert.view.CharacterGraphicComponent;
+
+/**
+ * This class models an enemy who jumps on the map moving randomly downwards.
+ */
+public class RedBall extends CharacterImpl {
+
+    /**
+     * @param startPos the first {@link Position2D} of the {@link Character} in the map
+     * @param speed the {@link Character} movement speed
+     * @param graphics the {@link Character}'s {@link CharacterGraphicComponent}
+     * @param standingTime the time passed on standing state
+     */
+    public RedBall(final Position2D startPos, final float speed, final CharacterGraphicComponent graphics, final int standingTime) {
+        super(startPos, speed, graphics);
+        this.setCurrentState(new MoveState.Fall(this));
+        this.setStandingState(new SimpleStandingState(this, standingTime));
+    }
+}
