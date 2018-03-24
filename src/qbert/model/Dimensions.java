613c868888937a67d84e6a4deffb21dc82b40a73
Andrea Giulianini <andreagiulianini97@gmail.com>
2018-03-24 12:56:36 +0100

    add variable in Dimensions class

--- src/qbert/model/Dimensions.java
+++ src/qbert/model/Dimensions.java
@@ -1,16 +1,19 @@
 package qbert.model;
 
+import qbert.model.utilities.Position2D;
+
 public class Dimensions {
     
     public static int screenHeight;
     public static int screenWidth;
-    
-    public static int getScreenHeight() {
-        return screenHeight;
-    }
-    
-    public static int getScreenWidth() {
-        return screenWidth;
-    }
+    public static int windowHeight;
+    public static int windowWidth;
+    public static int deathHeight;
+    public static int spawingHeight;
+    public static Position2D spawingPointLeft;
+    public static Position2D spawingPointRight;
+    public static int heightCube;
+    public static int heightTile;
+    public static int widthTile;
 
 }
--- src/qbert/model/Game.java
+++ src/qbert/model/Game.java
@@ -2,6 +2,12 @@ package qbert.model;
 
 import java.awt.Dimension;
 import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import javax.imageio.ImageIO;
+import qbert.model.utilities.Position2D;
 
 public class Game {
 
@@ -11,12 +17,22 @@ public class Game {
     private Level gameLevel;
    
     public Game() {
+        
         gameLevel = new Level();
         
         t = Toolkit.getDefaultToolkit();
         d = t.getScreenSize();
         Dimensions.screenHeight = d.height;
         Dimensions.screenWidth = d.width;
+        Dimensions.windowHeight = 1000;
+        Dimensions.windowWidth = 1500;
+        Dimensions.deathHeight = Dimensions.windowHeight+200;
+        Dimensions.spawingHeight = -100;
+        Dimensions.spawingPointLeft = new Position2D((Dimensions.screenWidth/2)-this.loadImg("/blueTile.png").getWidth(),-100);
+        Dimensions.spawingPointRight = new Position2D((Dimensions.screenWidth/2),-100);
+        Dimensions.heightCube = this.loadImg("/background.png").getHeight()/7;
+        Dimensions.heightTile = this.loadImg("/blueTile.png").getHeight();
+        Dimensions.widthTile = this.loadImg("/blueTile.png").getWidth();
     }
     
     public Level getLevel() {
@@ -25,5 +41,16 @@ public class Game {
     
     public void update( float elapsed) {
         gameLevel.update(elapsed);
+    }
+    
+    private BufferedImage loadImg(String path) {
+        BufferedImage res = null;
+        final URL spriteUrl = this.getClass().getResource(path);
+        try {
+            res = ImageIO.read(spriteUrl);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return res;
     }
 }
