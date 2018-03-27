42364920f62295d4c401e1e632440e8713985efc
Alessandro <alessandrostockman@gmail.com>
2018-03-27 11:19:35 +0200

    Refactor mapping based on dynamic dimension calculations

--- src/qbert/model/mapping/Mapper.java
+++ src/qbert/model/mapping/Mapper.java
@@ -10,53 +10,21 @@ import qbert.model.utilities.Position2D;
 
 public class Mapper {
 
-    private int screenWidth;
-    private int screenHeight;
-
-    //TODO: Calculate from sprite dimensions
-    private int tileWidth = 43;
-    private int tileHeight = 74;
     private int mapWidth = 601;
-    private int mapHeight = 499;
-
-    private int windowWidth;
-    private int windowHeight;
-
-    // TODO: Make these variables dynamic in some way.
-    private int centerX;
     private int topX;
     private int topY;
-    
-    //TODO: Remove
-    private int errorY = -6;
-    private int errorX = -43;
 
 
     public Mapper(int screenWidth, int screenHeight, int windowWidth, int windowHeight) {
-        this.screenWidth = screenWidth;
-        this.screenHeight = screenHeight;
-        this.windowWidth = windowWidth;
-        this.windowHeight = windowHeight;
-        
-        this.centerX = this.windowWidth / 2 - this.tileWidth;
-        this.topX = (this.windowWidth - this.mapWidth) / 2;
-        this.topY = (this.windowHeight - this.mapHeight) / 2;
+        this.topX = (Dimensions.windowWidth - this.mapWidth) / 2;
+        this.topY = (Dimensions.windowHeight - Dimensions.backgroundHeight) / 2;
     }
-
-//    public Position2D getPhysical(Position2D logicPos) {
-//        int oldX = (int) logicPos.getX();
-//        int oldY = (int) logicPos.getY();
-//        int newX = this.centerX + (-tileWidth) * (oldY - 1) + (tileWidth * 2) * (oldX - 1) - this.errorX;
-//        int newY = this.topY + tileHeight * (oldY - 1) - this.errorY;
-//
-//        return new Position2D(newX, newY);
-//    }
 
     public Position2D getPhysical(Position2D logicPos) {
         int oldX = (int) logicPos.getX();
         int oldY = (int) logicPos.getY();
-        int newX = this.topX + (this.tileWidth * (oldX + 1)) + this.errorX;
-        int newY = this.topY + Dimensions.backgroundHeight - (this.tileHeight * (oldY + 1)) + this.errorY;
+        int newX = this.topX + Dimensions.tileWidth / 2 * (oldX + 1) - Dimensions.tileWidth / 2;
+        int newY = this.topY + Dimensions.backgroundHeight - (Dimensions.cubeHeight * (oldY + 1)) - (Dimensions.tileHeight / 2);
 
         return new Position2D(newX, newY);
     }
