12c2209518048012e62003c67d81a2264b6ec3e0
Marco Meluzzi <marco.meluzzi@studio.unibo.it>
2018-04-03 15:17:44 +0200

    Add update method to Spawner

--- src/qbert/model/Spawner.java
+++ src/qbert/model/Spawner.java
@@ -1,46 +1,56 @@
 package qbert.model;
 
-import java.io.IOException;
-import java.net.URL;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Timer;
-
-import javax.imageio.ImageIO;
+import java.util.Map;
 
 import org.jdom2.JDOMException;
 
 import qbert.controller.LevelConfigurationReader;
-import qbert.model.utilities.Position2D;
 import qbert.view.QBertGraphicComponent;
 
-import java.util.List;
-
 public class Spawner {
+    
     private final Level level;
     private List<Character> charactersToSpawn;
+    private Map<String, LevelConfigurationReader.EnemyInfo> mapInfo = new HashMap<>();
+    
+    private int elapsedTime; /* */
     
     public Spawner(final Level level) {
         this.level = level;
+        final LevelConfigurationReader lcr = new LevelConfigurationReader();
         try {
-            this.charactersToSpawn = LevelConfigurationReader.readLevelConfiguration(level);
+            this.charactersToSpawn = lcr.readLevelConfiguration(level);
         } catch (JDOMException e) {
             e.printStackTrace();
         }
-        this.spawning();
+        this.mapInfo = lcr.getMapInfo();
+        this.spawnQbert();
     }
     
-    public void spawning() {
-        final URL spriteUrl = this.getClass().getResource("/QbertFrontStanding.png");
-        try {
-            level.spawn(new Qbert(Dimensions.spawingQBert, 0.35f, new QBertGraphicComponent(ImageIO.read(spriteUrl), Dimensions.spawingQBert)));
-            //level.spawn(new RedBall(Dimensions.spawingPointRight, 0.34f, new QBertGraphicComponent(ImageIO.read(spriteUrl), Dimensions.spawingPointRight), 1000));
-        } catch (IOException e) {
-            e.printStackTrace();
-        } 
-        
-        for (Character character : charactersToSpawn) {
-            level.spawn(character);
+    public void spawnQbert() {
+        level.spawn(new Qbert(Dimensions.spawingQBert, 0.35f, new QBertGraphicComponent(Sprites.qbertFrontStanding, Dimensions.spawingQBert)));
+    }
+    
+    public void update(final float dt) {
+        for (final Character character : this.charactersToSpawn) {
+            Class<?> cl = null;
+            try {
+                cl = Class.forName(character.getClass().getName());
+            } catch (ClassNotFoundException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            if (this.mapInfo.get(cl.getName()).getSpawningTime() <= this.elapsedTime) {
+                this.elapsedTime = 0;
+                if (this.mapInfo.get(cl.getName()).getCurrentQuantity() < this.mapInfo.get(cl.getName()).getTotalQuantity()) {
+                    level.spawn(character);
+                }
+                this.mapInfo.get(cl.getName()).incCurrentQuantity();
+            } else {
+                this.elapsedTime += dt;
+            }
         }
     }
-
 }
