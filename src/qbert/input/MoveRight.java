b948461a03a2fb6d8f1a347cfe343e864c2ba74c
ale <alessandro.lombardi5@studio.unibo.it>
2018-04-19 01:39:02 +0200

    Fix out of map QBert input behaviour

--- src/qbert/input/MoveRight.java
+++ src/qbert/input/MoveRight.java
@@ -2,6 +2,7 @@ package qbert.input;
 
 import qbert.model.Character;
 import qbert.model.Game;
+import qbert.model.Qbert;
 import qbert.model.states.MoveState;
 
 /**
@@ -12,7 +13,7 @@ public class MoveRight implements Command {
     @Override
     public void execute(final Game game) {
         final Character qbert = game.getLevel().getQBert();
-        if (!qbert.isMoving()) {
+        if (qbert.getCurrentState() instanceof Qbert.QbertStandingState) {
             qbert.setCurrentState(new MoveState.DownRight(qbert));
         }
     }
