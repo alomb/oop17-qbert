fdd26f91fb344cdb36590f07161273b880286e56
AndreaGiulianini <andreagiulianini97@gmail.com>
2018-06-09 18:18:02 +0200

    java heap space bug

--- src/qbert/model/Game.java
+++ src/qbert/model/Game.java
@@ -46,95 +46,93 @@ public class Game {
         t = Toolkit.getDefaultToolkit();
         d = t.getScreenSize();
         try {
-          
-          if (!qbert.temp.Config.DEBUG || new File("../img/png/").listFiles().length < new File("res/svg/").listFiles().length) {
-          
-              for (File file: new File("../img/png/").listFiles()) {
-                  if (!file.isDirectory()) {
-                      file.delete();
-                  }
-              }
-    
-              convertSvgToPng("res/svg/BackgroundBlue.svg", "../img/png/BackgroundBlue.png"); 
-              convertSvgToPng("res/svg/BackgroundBrown.svg", "../img/png/BackgroundBrown.png"); 
-              convertSvgToPng("res/svg/BackgroundGreen.svg", "../img/png/BackgroundGreen.png"); 
-              convertSvgToPng("res/svg/BackgroundGrey.svg", "../img/png/BackgroundGrey.png"); 
-              convertSvgToPng("res/svg/CoilyBackMove.svg", "../img/png/CoilyBackMove.png"); 
-              convertSvgToPng("res/svg/CoilyBackStand.svg", "../img/png/CoilyBackStand.png"); 
-              convertSvgToPng("res/svg/CoilyFrontMove.svg", "../img/png/CoilyFrontMove.png"); 
-              convertSvgToPng("res/svg/CoilyFrontStand.svg", "../img/png/CoilyFrontStand.png");
-              convertSvgToPng("res/svg/GreenBallMove.svg", "../img/png/GreenBallMove.png"); 
-              convertSvgToPng("res/svg/GreenBallStand.svg", "../img/png/GreenBallStand.png");
-              convertSvgToPng("res/svg/PurpleBallMove.svg", "../img/png/PurpleBallMove.png"); 
-              convertSvgToPng("res/svg/PurpleBallStand.svg", "../img/png/PurpleBallStand.png");
-              convertSvgToPng("res/svg/QbertBackMove.svg", "../img/png/QbertBackMove.png"); 
-              convertSvgToPng("res/svg/QbertBackStand.svg", "../img/png/QbertBackStand.png"); 
-              convertSvgToPng("res/svg/QbertFrontMove.svg", "../img/png/QbertFrontMove.png"); 
-              convertSvgToPng("res/svg/QbertFrontStand.svg", "../img/png/QbertFrontStand.png");
-              convertSvgToPng("res/svg/QbertDead.svg", "../img/png/QbertDead.png");
-              convertSvgToPng("res/svg/QbertOnDisk.svg", "../img/png/QbertOnDisk.png"); 
-              convertSvgToPng("res/svg/RedBallMove.svg", "../img/png/RedBallMove.png"); 
-              convertSvgToPng("res/svg/RedBallStand.svg", "../img/png/RedBallStand.png");
-              convertSvgToPng("res/svg/TileBeige.svg", "../img/png/TileBeige.png");
-              convertSvgToPng("res/svg/TileBlue.svg", "../img/png/TileBlue.png");
-              convertSvgToPng("res/svg/TileGreen.svg", "../img/png/TileGreen.png");
-              convertSvgToPng("res/svg/TileGrey.svg", "../img/png/TileGrey.png");
-              convertSvgToPng("res/svg/TilePink.svg", "../img/png/TilePink.png");
-              convertSvgToPng("res/svg/TilePurple.svg", "../img/png/TilePurple.png");
-              convertSvgToPng("res/svg/TileYellow.svg", "../img/png/TileYellow.png");
-              convertSvgToPng("res/svg/Life.svg", "../img/png/Life.png");
-              convertSvgToPng("res/svg/Disk1.svg", "../img/png/Disk1.png");
-              convertSvgToPng("res/svg/Disk2.svg", "../img/png/Disk2.png");
-              convertSvgToPng("res/svg/Disk3.svg", "../img/png/Disk3.png");
-              convertSvgToPng("res/svg/Disk4.svg", "../img/png/Disk4.png");
-              convertSvgToPng("res/svg/SamMove.svg", "../img/png/SamMove.png");
-              convertSvgToPng("res/svg/SamStand.svg", "../img/png/SamStand.png");
-              convertSvgToPng("res/svg/SlickMove.svg", "../img/png/SlickMove.png");
-              convertSvgToPng("res/svg/SlickStand.svg", "../img/png/SlickStand.png");
-
+            if(new File("../img/png/").listFiles().length != new File("res/svg/").listFiles().length) {
+                for (File file: new File("../img/png/").listFiles()) {
+                    if (!file.isDirectory()) {
+                        file.delete();
+                    }
+                }
+      
+                convertSvgToPng("res/svg/BackgroundBlue.svg", "../img/png/BackgroundBlue.png"); 
+                convertSvgToPng("res/svg/BackgroundBrown.svg", "../img/png/BackgroundBrown.png"); 
+                convertSvgToPng("res/svg/BackgroundGreen.svg", "../img/png/BackgroundGreen.png"); 
+                convertSvgToPng("res/svg/BackgroundGrey.svg", "../img/png/BackgroundGrey.png"); 
+                convertSvgToPng("res/svg/CoilyBackMove.svg", "../img/png/CoilyBackMove.png"); 
+                convertSvgToPng("res/svg/CoilyBackStand.svg", "../img/png/CoilyBackStand.png"); 
+                convertSvgToPng("res/svg/CoilyFrontMove.svg", "../img/png/CoilyFrontMove.png"); 
+                convertSvgToPng("res/svg/CoilyFrontStand.svg", "../img/png/CoilyFrontStand.png");
+                convertSvgToPng("res/svg/GreenBallMove.svg", "../img/png/GreenBallMove.png"); 
+                convertSvgToPng("res/svg/GreenBallStand.svg", "../img/png/GreenBallStand.png");
+                convertSvgToPng("res/svg/PurpleBallMove.svg", "../img/png/PurpleBallMove.png"); 
+                convertSvgToPng("res/svg/PurpleBallStand.svg", "../img/png/PurpleBallStand.png");
+                convertSvgToPng("res/svg/QbertBackMove.svg", "../img/png/QbertBackMove.png"); 
+                convertSvgToPng("res/svg/QbertBackStand.svg", "../img/png/QbertBackStand.png"); 
+                convertSvgToPng("res/svg/QbertFrontMove.svg", "../img/png/QbertFrontMove.png"); 
+                convertSvgToPng("res/svg/QbertFrontStand.svg", "../img/png/QbertFrontStand.png");
+                convertSvgToPng("res/svg/QbertDead.svg", "../img/png/QbertDead.png");
+                convertSvgToPng("res/svg/QbertOnDisk.svg", "../img/png/QbertOnDisk.png"); 
+                convertSvgToPng("res/svg/RedBallMove.svg", "../img/png/RedBallMove.png"); 
+                convertSvgToPng("res/svg/RedBallStand.svg", "../img/png/RedBallStand.png");
+                convertSvgToPng("res/svg/TileBeige.svg", "../img/png/TileBeige.png");
+                convertSvgToPng("res/svg/TileBlue.svg", "../img/png/TileBlue.png");
+                convertSvgToPng("res/svg/TileGreen.svg", "../img/png/TileGreen.png");
+                convertSvgToPng("res/svg/TileGrey.svg", "../img/png/TileGrey.png");
+                convertSvgToPng("res/svg/TilePink.svg", "../img/png/TilePink.png");
+                convertSvgToPng("res/svg/TilePurple.svg", "../img/png/TilePurple.png");
+                convertSvgToPng("res/svg/TileYellow.svg", "../img/png/TileYellow.png");
+                convertSvgToPng("res/svg/Life.svg", "../img/png/Life.png");
+                convertSvgToPng("res/svg/Disk1.svg", "../img/png/Disk1.png");
+                convertSvgToPng("res/svg/Disk2.svg", "../img/png/Disk2.png");
+                convertSvgToPng("res/svg/Disk3.svg", "../img/png/Disk3.png");
+                convertSvgToPng("res/svg/Disk4.svg", "../img/png/Disk4.png");
+                convertSvgToPng("res/svg/SamMove.svg", "../img/png/SamMove.png");
+                convertSvgToPng("res/svg/SamStand.svg", "../img/png/SamStand.png");
+                convertSvgToPng("res/svg/SlickMove.svg", "../img/png/SlickMove.png");
+                convertSvgToPng("res/svg/SlickStand.svg", "../img/png/SlickStand.png");
+            }
               
-          }
+
         } catch (Exception e) {
             System.out.println("Error convert " + e.getMessage());
         }
 
         try {
-          Sprites.blueBackground = loadImg("../img/png/BackgroundBlue.png");
-          Sprites.brownBackground = loadImg("../img/png/BackgroundBrown.png");
-          Sprites.greenBackground = loadImg("../img/png/BackgroundGreen.png");
-          Sprites.greyBackground = loadImg("../img/png/BackgroundGrey.png");
-          Sprites.coilyBackMoving = loadImg("../img/png/CoilyBackMove.png");
-          Sprites.coilyBackStanding = loadImg("../img/png/CoilyBackStand.png");
-          Sprites.coilyFrontMoving = loadImg("../img/png/CoilyFrontMove.png");
-          Sprites.coilyFrontStanding = loadImg("../img/png/CoilyFrontStand.png");
-          Sprites.greenBallMoving = loadImg("../img/png/GreenBallMove.png");
-          Sprites.greenBallStanding = loadImg("../img/png/GreenBallStand.png");
-          Sprites.purpleBallMoving = loadImg("../img/png/PurpleBallMove.png");
-          Sprites.purpleBallStanding = loadImg("../img/png/PurpleBallStand.png");
-          Sprites.samStanding = loadImg("../img/png/SamStand.png");
-          Sprites.samMoving = loadImg("../img/png/SamMove.png");
-          Sprites.slickStanding = loadImg("../img/png/SlickStand.png");
-          Sprites.slickMoving = loadImg("../img/png/SlickMove.png");
-          Sprites.disk1 = loadImg("../img/png/Disk1.png");
-          Sprites.disk2 = loadImg("../img/png/Disk2.png");
-          Sprites.disk3 = loadImg("../img/png/Disk3.png");
-          Sprites.disk4 = loadImg("../img/png/Disk4.png");
-          Sprites.qbertBackMoving = loadImg("../img/png/QbertBackMove.png"); 
-          Sprites.qbertBackStanding = loadImg("../img/png/QbertBackStand.png");
-          Sprites.qbertFrontMoving = loadImg("../img/png/QbertFrontMove.png"); 
-          Sprites.qbertFrontStanding = loadImg("../img/png/QbertFrontStand.png");
-          Sprites.qbertDead = loadImg("../img/png/QbertDead.png");
-          Sprites.qbertOnDisk = loadImg("../img/png/QbertOnDisk.png");
-          Sprites.redBallMoving = loadImg("../img/png/RedBallMove.png");
-          Sprites.redBallStanding = loadImg("../img/png/RedBallStand.png");
-          Sprites.beigeTile = loadImg("../img/png/TileBeige.png");
-          Sprites.blueTile = loadImg("../img/png/TileBlue.png");
-          Sprites.greenTile = loadImg("../img/png/TileGreen.png"); 
-          Sprites.greyTile = loadImg("../img/png/TileGrey.png");
-          Sprites.pinkTile = loadImg("../img/png/TilePink.png");
-          Sprites.purpleTile = loadImg("../img/png/TilePurple.png");
-          Sprites.yellowTile = loadImg("../img/png/TileYellow.png");
-          Sprites.life = loadImg("../img/png/Life.png"); 
+              Sprites.blueBackground = loadImg("../img/png/BackgroundBlue.png");
+              Sprites.brownBackground = loadImg("../img/png/BackgroundBrown.png");
+              Sprites.greenBackground = loadImg("../img/png/BackgroundGreen.png");
+              Sprites.greyBackground = loadImg("../img/png/BackgroundGrey.png");
+              Sprites.coilyBackMoving = loadImg("../img/png/CoilyBackMove.png");
+              Sprites.coilyBackStanding = loadImg("../img/png/CoilyBackStand.png");
+              Sprites.coilyFrontMoving = loadImg("../img/png/CoilyFrontMove.png");
+              Sprites.coilyFrontStanding = loadImg("../img/png/CoilyFrontStand.png");
+              Sprites.greenBallMoving = loadImg("../img/png/GreenBallMove.png");
+              Sprites.greenBallStanding = loadImg("../img/png/GreenBallStand.png");
+              Sprites.purpleBallMoving = loadImg("../img/png/PurpleBallMove.png");
+              Sprites.purpleBallStanding = loadImg("../img/png/PurpleBallStand.png");
+              Sprites.samStanding = loadImg("../img/png/SamStand.png");
+              Sprites.samMoving = loadImg("../img/png/SamMove.png");
+              Sprites.slickStanding = loadImg("../img/png/SlickStand.png");
+              Sprites.slickMoving = loadImg("../img/png/SlickMove.png");
+              Sprites.disk1 = loadImg("../img/png/Disk1.png");
+              Sprites.disk2 = loadImg("../img/png/Disk2.png");
+              Sprites.disk3 = loadImg("../img/png/Disk3.png");
+              Sprites.disk4 = loadImg("../img/png/Disk4.png");
+              Sprites.qbertBackMoving = loadImg("../img/png/QbertBackMove.png"); 
+              Sprites.qbertBackStanding = loadImg("../img/png/QbertBackStand.png");
+              Sprites.qbertFrontMoving = loadImg("../img/png/QbertFrontMove.png"); 
+              Sprites.qbertFrontStanding = loadImg("../img/png/QbertFrontStand.png");
+              Sprites.qbertDead = loadImg("../img/png/QbertDead.png");
+              Sprites.qbertOnDisk = loadImg("../img/png/QbertOnDisk.png");
+              Sprites.redBallMoving = loadImg("../img/png/RedBallMove.png");
+              Sprites.redBallStanding = loadImg("../img/png/RedBallStand.png");
+              Sprites.beigeTile = loadImg("../img/png/TileBeige.png");
+              Sprites.blueTile = loadImg("../img/png/TileBlue.png");
+              Sprites.greenTile = loadImg("../img/png/TileGreen.png"); 
+              Sprites.greyTile = loadImg("../img/png/TileGrey.png");
+              Sprites.pinkTile = loadImg("../img/png/TilePink.png");
+              Sprites.purpleTile = loadImg("../img/png/TilePurple.png");
+              Sprites.yellowTile = loadImg("../img/png/TileYellow.png");
+              Sprites.life = loadImg("../img/png/Life.png"); 
 
         } catch (Exception e) {
             System.out.println("Error load " + e.toString());
@@ -171,7 +169,8 @@ public class Game {
 
     private BufferedImage loadImg(String pathOut) throws Exception{
         BufferedImage res = null;
-        final URL spriteUrl = this.getClass().getResource("/png/" + pathOut.split("/")[3]);
+        final URL spriteUrl = new URL(new URL("file:"),pathOut);
+        System.out.println(spriteUrl.toString());
         try {
             res = ImageIO.read(spriteUrl);
         } catch (IOException e) {
@@ -194,8 +193,8 @@ public class Game {
         final Element root = document.getRootElement();
         float width = Float.valueOf(root.getAttributeValue("width"));
         float height = Float.valueOf(root.getAttributeValue("height"));
-        converter.addTranscodingHint(PNGTranscoder.KEY_WIDTH, new Float(1 * width));
-        converter.addTranscodingHint(PNGTranscoder.KEY_HEIGHT, new Float(1 * height));
+        converter.addTranscodingHint(PNGTranscoder.KEY_WIDTH, new Float(20*width));
+        converter.addTranscodingHint(PNGTranscoder.KEY_HEIGHT, new Float(20*height));
         converter.addTranscodingHint(PNGTranscoder.KEY_AOI, new Rectangle(0, 0, Math.round(width), Math.round(height)));
 
         converter.transcode(imageSvg, outputImagePng);
